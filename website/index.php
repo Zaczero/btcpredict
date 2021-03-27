6e3e9541c50886ff63e8698729dbcfecd444ae41
Kamil Monicz <kamil@monicz.pl>
2021-03-27 22:43:25 +0100

    Search engine optimizations

--- website/index.php
+++ website/index.php
@@ -47,6 +47,7 @@ $prediction_generated_one = date(DATE_FORMAT, $json['current_date']);
         <meta name="msapplication-config" content="/browserconfig.xml">
 
         <title>BTC Predict - Current bull run price and date prediction</title>
+        <meta name="description" content="Bitcoin bull run prediction project which aims to evaluate current bull run's top price alongside the exact date." />
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light">
